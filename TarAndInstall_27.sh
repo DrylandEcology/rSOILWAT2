5a1340ecace3d7c110ce4cc58f361307be9019fe
Kyle Taylor <ktaylora@mdefault.local>
2014-08-01 13:27:26 -0600

    adding a fix to TarandInstall_27.sh so it will work on Mac OS X.

--- TarAndInstall_27.sh
+++ TarAndInstall_27.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-cd ../
-#tar -pczf Rsoilwat_v27.tar.gz Rsoilwat_v27
-R CMD BUILD Rsoilwat*
-mv Rsoilwat_0.27* Rsoilwat_v27.tar.gz
-R CMD INSTALL Rsoilwat_v27.tar.gz
-rm Rsoilwat_v27.tar.gz
+oldDir=$PWD
+# For Linux
+if [ `uname | awk '{ print $1 }'` = "Linux" ]; then
+  cd ../
+  tar -c Rsoilwat_v27 >> Rsoilwat_v27.tar
+  gzip -c Rsoilwat_v27.tar >> Rsoilwat_v27.tar.gz
+  rm -rf Rsoilwat_v27.tar
+  R CMD INSTALL Rsoilwat_v27.tar.gz;
+  rm -rf Rsoilwat_v27.tar.gz;
+  cd $oldDir
+fi
+# For MacOS X
+if [ `uname | awk '{ print $1 }'` = "Darwin" ]; then
+  wd=`pwd | awk -F"/" '{print $NF}'`
+  cd src/
+  make clean;
+  cd ../../
+  R CMD INSTALL $wd
+  cd $oldDir
+fi
