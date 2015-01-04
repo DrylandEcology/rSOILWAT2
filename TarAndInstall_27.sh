de69300262cb30a587abd2cce20f97eee292848a
Kyle Taylor <ktaylora@users.noreply.github.com>
2015-01-04 10:35:04 -0700

    Update TarAndInstall_27.sh
    house-keeping.

--- TarAndInstall_27.sh
+++ TarAndInstall_27.sh
@@ -3,11 +3,11 @@ oldDir=$PWD
 # For Linux
 if [ `uname | awk '{ print $1 }'` = "Linux" ]; then
   cd ../
-  tar -c Rsoilwat_v27 >> Rsoilwat_v27.tar
-  gzip -c Rsoilwat_v27.tar >> Rsoilwat_v27.tar.gz
-  rm -rf Rsoilwat_v27.tar
-  R CMD INSTALL Rsoilwat_v27.tar.gz;
-  rm -rf Rsoilwat_v27.tar.gz;
+  tar -c Rsoilwat >> Rsoilwat.tar
+  gzip -c Rsoilwat.tar >> Rsoilwat.tar.gz
+  rm -rf Rsoilwat.tar
+  R CMD INSTALL Rsoilwat.tar.gz;
+  rm -rf Rsoilwat.tar.gz;
   cd $oldDir
 fi
 # For MacOS X
