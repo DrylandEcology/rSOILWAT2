a2b634e00bb02422609df5d143bd79bd83efe14a
dschlaep <daniel.schlaepfer@unibas.ch>
2015-04-16 17:09:51 +0200

    fixed interception output for forbs

---
+++ TarAndInstall_31n.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+oldDir=$PWD
+# For Linux
+if [ `uname | awk '{ print $1 }'` = "Linux" ]; then
+  cd ../
+  tar -c Rsoilwat_v31 >> Rsoilwat.tar
+  gzip -c Rsoilwat.tar >> Rsoilwat.tar.gz
+  rm -rf Rsoilwat.tar
+  R CMD INSTALL Rsoilwat.tar.gz;
+  rm -rf Rsoilwat.tar.gz;
+  cd $oldDir
+fi
+# For MacOS X
+if [ `uname | awk '{ print $1 }'` = "Darwin" ]; then
+  wd=`pwd | awk -F"/" '{print $NF}'`
+  cd src/
+  make clean;
+  cd ../../
+  R CMD INSTALL $wd
+  cd $oldDir
+fi
