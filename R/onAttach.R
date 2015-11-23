b8e9d842bc1c617f91661d3f7e00afb03c43b481
dschlaep <daniel.schlaepfer@unibas.ch>
2015-11-23 09:22:31 +0100

    Pulled down latest commit 'd7293ed' to submodule SOILWAT and increased subversion number of Rsoilwat

--- R/onAttach.R
+++ R/onAttach.R
@@ -4,7 +4,7 @@
   {
     meta <- packageDescription("Rsoilwat31")
     packageStartupMessage("Package 'Rsoilwat_31', ", meta$Version, " (", meta$Date, ").")
-	print("Fixed soil temperature under increased depth of constant soil temperature")
+	print("Calculates surface temperature under snow cover based on Koren et al. 1999 JGR")
   }
   invisible()
 }
