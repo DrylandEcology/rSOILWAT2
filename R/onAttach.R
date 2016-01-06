12368614b631d0a532618e2eb1479bd569f9d845
dschlaep <daniel.schlaepfer@unibas.ch>
2016-01-06 23:44:19 +0100

    Pulled down latest commit '3939e9e'  to submodule SoilWat (updated frozen/unfrozen soil)

--- R/onAttach.R
+++ R/onAttach.R
@@ -4,8 +4,7 @@
   {
     meta <- packageDescription("Rsoilwat31")
     packageStartupMessage("Package 'Rsoilwat_31', ", meta$Version, " (", meta$Date, ").")
-	print("Fixed soil temperature bug (strong dampening with depth): SWC - VWC confusion")
-	print("NOTE: soil thawing/freezing turned off due to large number of errors - NEEDS FIX")
+	print("Replaced frozen/unfrozen soil routine")
   }
   invisible()
 }
