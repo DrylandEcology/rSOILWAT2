a477c9d0b895bb373cc62fc48b8f7c4bc0595b44
dschlaep <daniel.schlaepfer@unibas.ch>
2016-01-11 17:40:46 +0100

    Improved help files and added full GPL-3 license information
    - updated help pages to refer to current function names (several were
    defunct from Rsoilwat)
    - inst/extdata contains files of a SOILWAT example project
    - help pages use these files for demonstration
    - added references to publications
    - TODO: help pages need additional careful editing

--- R/Rsw.R
+++ R/Rsw.R
@@ -1,5 +1,23 @@
-print("Rsw")
-sw_exec <- function(inputData=NULL,weatherList=NULL,dir="", files.in="files_v30.in", echo=FALSE, quiet=FALSE) {
+###############################################################################
+#Rsoilwat and Rsoilwat31
+#    Copyright (C) {2009-2016}  {Ryan Murphy, Daniel Schlaepfer, William Lauenroth, John Bradford}
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+
+sw_exec <- function(inputData=NULL,weatherList=NULL,dir="", files.in="files_v31.in", echo=FALSE, quiet=FALSE) {
 	input <- c("sw_v27")
 	if(dir!="")
 		input<-c(input,"-d", dir)
