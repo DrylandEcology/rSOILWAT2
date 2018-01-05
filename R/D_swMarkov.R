6245b6fd5bdb1cc08abd5f583780ffa3ab5c4772
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2018-01-05 11:57:54 -0500

    Fix S4 class swMarkov and its methods
    - fix method `initialize` so that arguments can be passed to `new()`
    and guarantee correct dimnames
    - fix `validity` method
    - validate object in replacement methods
    - added accessor and replacement methods for signature "swMarkov"

--- R/D_swMarkov.R
+++ R/D_swMarkov.R
@@ -1,6 +1,6 @@
 ###############################################################################
 #rSOILWAT2
-#    Copyright (C) {2009-2016}  {Ryan Murphy, Daniel Schlaepfer, William Lauenroth, John Bradford}
+#    Copyright (C) {2009-2018}  {Ryan Murphy, Daniel Schlaepfer, William Lauenroth, John Bradford}
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -17,9 +17,7 @@
 ###############################################################################
 
 
-# TODO: Add comment
-#
-# Author: ryan
+# Author: Ryan J. Murphy (2013); Daniel R Schlaepfer (2013-2018)
 ###############################################################################
 
 
@@ -29,6 +27,8 @@ setClass("swMarkov", slots = c(Prob = "matrix", Conv = "matrix"))
 
 setMethod("initialize", signature = "swMarkov", function(.Object, ...) {
   def <- slot(inputData, "markov")
+  dots <- list(...)
+  dns <- names(dots)
 
   # We don't set values for slots `Prob` and `Conv`; this is to prevent simulation runs with
   # accidentally incorrect values
@@ -36,19 +36,76 @@ setMethod("initialize", signature = "swMarkov", function(.Object, ...) {
   # We have to explicitly give column names (as defined in `onGet_MKV_prob` and
   # `onGet_MKV_conv`) because they are not read in by C code if the weather generator is
   # turned off
-  ctemp <- c("day", "wet", "dry", "avg_ppt", "std_ppt")
-  temp <- matrix(NA_real_, nrow = 366, ncol = length(ctemp), dimnames = list(NULL, ctemp))
-  temp[, "day"] <- 1:366
+  ctemp_Prob <- c("day", "wet", "dry", "avg_ppt", "std_ppt")
+  ctemp_Conv <- c("week", "t1", "t2", "t3", "t4", "t5", "t6")
+
+  if ("Prob" %in% dns) {
+    temp <- dots[["Prob"]]
+    if (sum(dim(temp)) > 0) {
+      colnames(temp) <- ctemp_Prob
+    }
+  } else {
+    temp <- matrix(NA_real_, nrow = 366, ncol = length(ctemp_Prob),
+      dimnames = list(NULL, ctemp_Prob))
+    temp[, "day"] <- 1:366
+  }
   .Object@Prob <- temp
 
-  ctemp <- c("week", "t1", "t2", "t3", "t4", "t5", "t6")
-  temp <- matrix(NA_real_, nrow = 53, ncol = length(ctemp), dimnames = list(NULL, ctemp))
-  temp[, "week"] <- 1:53
+  if ("Conv" %in% dns) {
+    temp <- dots[["Conv"]]
+    if (sum(dim(temp)) > 0) {
+      colnames(temp) <- ctemp_Conv
+    }
+  } else {
+    temp <- matrix(NA_real_, nrow = 53, ncol = length(ctemp_Conv),
+      dimnames = list(NULL, ctemp_Conv))
+    temp[, "week"] <- 1:53
+  }
   .Object@Conv <- temp
 
   #.Object <- callNextMethod(.Object, ...) # not needed because no relevant inheritance
   validObject(.Object)
   .Object
+})
+
+swMarkov_validity <- function(object) {
+  val <- TRUE
+
+  temp <- dim(object@Prob)
+  if (!(temp == c(0, 0) || temp == c(366, 5))) {
+    msg <- paste("@Prob must be a 0x0 or a 366x5 matrix.")
+    val <- if (isTRUE(val)) msg else c(val, msg)
+  }
+
+  temp <- dim(object@Conv)
+  if (!(temp == c(0, 0) || temp == c(53, 7))) {
+    msg <- paste("@Conv must be a 0x0 or a 53x7 matrix.")
+    val <- if (isTRUE(val)) msg else c(val, msg)
+  }
+
+  val
+}
+setValidity("swMarkov", swMarkov_validity)
+
+
+setMethod("get_Markov", "swMarkov", function(object) object)
+setMethod("swMarkov_Prob", "swMarkov", function(object) object@Prob)
+setMethod("swMarkov_Conv", "swMarkov", function(object) object@Conv)
+
+setReplaceMethod("set_Markov", signature = "swMarkov", function(object, value) {
+  object <- value
+  validObject(object)
+  object
+})
+setReplaceMethod("swMarkov_Prob", signature = "swMarkov", function(object, value) {
+  object@Prob <- value
+  validObject(object)
+  object
+})
+setReplaceMethod("swMarkov_Conv", signature = "swMarkov", function(object, value) {
+  object@Conv <- value
+  validObject(object)
+  object
 })
 
 
