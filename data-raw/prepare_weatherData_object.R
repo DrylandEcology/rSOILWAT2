1440e7fee68cb0105a8a35e5c0b794be5fc836db
dschlaep <daniel.schlaepfer@alumni.ethz.ch>
2019-03-01 08:54:30 -0500

    Remove `devtools` package from code as much as possible
    - use package `use_this` in script `prepare_weatherData.object.R`
    - only use package `spelling` in unit tests `test_rSOILWAT2_Spelling.R`
    
    - remaining use in function `pkg_temp_dir` of unit tests `test_helper.R` code

--- data-raw/prepare_weatherData_object.R
+++ data-raw/prepare_weatherData_object.R
@@ -6,4 +6,4 @@ dir_definf <- file.path("data-raw", "weatherData")
 weatherData <- rSOILWAT2::getWeatherData_folders("data-raw", "weatherData",
   "weath")
 
-devtools::use_data(weatherData, internal = FALSE)
+usethis::use_data(weatherData, internal = FALSE)
