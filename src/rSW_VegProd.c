DrylandEcology/rSOILWAT2

commit f66238c81b6e063d2c6397509643b926766527f0

Function `onSet_SW_VPD` calls new SOILWAT2 function `SW_VPD_fix_cover`
- replace a copy of the code with a call to the new function

src/rSW_VegProd.c
@@ -623,21 +623,7 @@ void onSet_SW_VPD(SEXP SW_VPD) {
 	v->veg[SW_FORBS].co2_wue_coeff2 = REAL(CO2Coefficients)[15];
 
 
-	fraction_sum = v->veg[SW_GRASS].cov.fCover + v->veg[SW_SHRUB].cov.fCover + v->veg[SW_TREES].cov.fCover + v->veg[SW_FORBS].cov.fCover + v->bare_cov.fCover;
-	if (!EQ(fraction_sum, 1.0)) {
-		LogError(logfp, LOGWARN, "%s : Fractions of vegetation components were normalized, "
-			"sum of fractions (%5.4f) != 1.0.\nNew coefficients are:", MyFileName, fraction_sum);
-		v->veg[SW_GRASS].cov.fCover /= fraction_sum;
-		v->veg[SW_SHRUB].cov.fCover /= fraction_sum;
-		v->veg[SW_TREES].cov.fCover /= fraction_sum;
-		v->bare_cov.fCover /= fraction_sum;
-		v->veg[SW_FORBS].cov.fCover /= fraction_sum;
-		LogError(logfp, LOGWARN, "  Grassland fraction : %5.4f", v->veg[SW_GRASS].cov.fCover);
-		LogError(logfp, LOGWARN, "  Shrubland fraction : %5.4f", v->veg[SW_SHRUB].cov.fCover);
-		LogError(logfp, LOGWARN, "  Forest/tree fraction : %5.4f", v->veg[SW_TREES].cov.fCover);
-		LogError(logfp, LOGWARN, "  FORB fraction : %5.4f", v->veg[SW_FORBS].cov.fCover);
-		LogError(logfp, LOGWARN, "  Bare Ground fraction : %5.4f", v->bare_cov.fCover);
-	}
+  SW_VPD_fix_cover();
 
 	SW_VPD_init();
 

commit 9b8ef18ec64480f1e81bbdfd5c879fec00e68445

Fix warning arising from function `onSet_SW_VPD`
Problem was: "rSW_VegProd.c:441:8: warning: unused variable 'fraction_sum' [-Wunused-variable]
           RealD fraction_sum = 0.;"

This was caused by commit https://github.com/DrylandEcology/rSOILWAT2/commit/f66238c81b6e063d2c6397509643b926766527f0 which removed the need for variable `fraction_sum` by using a call to the function `SW_VPD_fix_cover` -- but forgot to remove `fraction_sum`

src/rSW_VegProd.c
@@ -438,7 +438,6 @@ void onSet_SW_VPD(SEXP SW_VPD) {
 	SEXP MonthlyVeg, Grasslands, Shrublands, Forest, Forb;
 	SEXP CO2Coefficients;
 	RealD *p_Grasslands, *p_Shrublands, *p_Forest, *p_Forb;
-	RealD fraction_sum = 0.;
 
 	MyFileName = SW_F_name(eVegProd);
 