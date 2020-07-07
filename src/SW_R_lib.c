c5d16584b6fc4dc125c22e0aa73cd3b07f36d1a8
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2020-07-07 12:46:22 -0400

    Update SOILWAT2 submodule to reflect fixed handling of missing aspect values

--- src/SW_R_lib.c
+++ src/SW_R_lib.c
@@ -140,7 +140,7 @@ SEXP onGetInputDataFromFiles(SEXP inputOptions) {
 
   // initialize simulation run (based on user inputs)
   #ifdef RSWDEBUG
-  if (debug) swprintf(" init simulation run ...");
+  if (debug) swprintf(" init simulation run ...\n");
   #endif
   SW_CTL_init_run();
 
@@ -224,12 +224,12 @@ SEXP onGetInputDataFromFiles(SEXP inputOptions) {
 
   // de-allocate all memory, but `p_OUT`
   #ifdef RSWDEBUG
-  if (debug) swprintf("De-allocate most memory\n");
+  if (debug) swprintf(" > de-allocate most memory; \n");
   #endif
   SW_CTL_clear_model(FALSE);
 
   #ifdef RSWDEBUG
-  if (debug) swprintf(" completed.\n");
+  if (debug) swprintf(" onGetInputDataFromFiles completed.\n");
   #endif
 
   UNPROTECT(5);
