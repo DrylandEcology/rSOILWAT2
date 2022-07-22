2c64be51116b32584d085f572e68dcc85954547e
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2022-07-22 09:01:53 -0400

    `onSet_WTH_DATA()` copies now all weather data from rSOILWAT2 to SOILWAT2
    - previously, the code called `readAllWeather()` which reads weather values from disk; however, this was incorrect behavior
    - `onSet_WTH_DATA()` (previously, `onSet_SW_WTH_read()`) has now equivalent functionality to `SW_WTH_read()`with the difference that SOILWAT2 `allHist` is filled with values that are copied from `rSOILWAT2` list of `swWeatherData` instead of being read from files on disk.

--- src/rSW_Weather.c
+++ src/rSW_Weather.c
@@ -52,7 +52,12 @@ static char *cSW_WTH_names[] = {
 /* --------------------------------------------------- */
 
 static SEXP onGet_WTH_DATA_YEAR(TimeInt year);
-
+static void rSW2_passAllWeather(
+  SEXP listAllW,
+  SW_WEATHER_HIST **allHist,
+  int startYear,
+  unsigned int n_years
+);
 
 
 /* =================================================== */
@@ -284,29 +289,181 @@ SEXP onGet_WTH_DATA_YEAR(TimeInt year) {
 
 
 /**
-  @brief Copy all weather data from `rSOILWAT2` list of `swWeatherData` to
-    `SOILWAT2` `allHist`
+  @brief Move all weather data from `rSOILWAT2` to `SOILWAT2`, impute
+    missing values (weather generator), and apply scaling parameters
 
-  @note However, currently, it creates `allHist` and
-    reads all data from files instead.
+  Equivalent functionality to `SW_WTH_read()`with the difference that
+  `SOILWAT2` `allHist` is filled with values that are
+  copied from `rSOILWAT2` list of `swWeatherData`
+  instead of being read from files on disk.
+
 
   Called by `rSW_CTL_obtain_inputs()` if `from_files` is `FALSE`.
+
+  @note Elements `endyr` and `startyr` of `SW_Model` must be set/updated
+    via `onSet_SW_MDL()` before this function is called.
 */
 void onSet_WTH_DATA(void) {
 
-    int year;
+  SEXP listAllWeather;
 
-    SW_WEATHER *w = &SW_Weather;
-    SW_MODEL *m = &SW_Model;
+  // Determine which `rSOILWAT2` list of `swWeatherData` we are using
+  listAllWeather = bWeatherList ?
+    WeatherList :
+    GET_SLOT(InputData, install("weatherHistory"));
 
-    w->n_years = m->endyr - m->startyr + 1;
 
-    w->allHist = (SW_WEATHER_HIST **)malloc(sizeof(SW_WEATHER_HIST *) * w->n_years);
+  // Deallocate (previous, if any) `allHist`
+  // (using value of `SW_Weather.n_years` previously used to allocate)
+  deallocateAllWeather();
 
-    for(year = 0; year < w->n_years; year++) {
-        w->allHist[year] = (SW_WEATHER_HIST *)malloc(sizeof(SW_WEATHER_HIST));
+  // Determine required size of new `allHist`
+  SW_Weather.n_years = SW_Model.endyr - SW_Model.startyr + 1;
+
+  // Allocate new `allHist` (based on current `SW_Weather.n_years`)
+  allocateAllWeather();
+
+
+  // Equivalent to `readAllWeather()`:
+  // fill `SOILWAT2` `allHist` with values from `rSOILWAT2`
+  rSW2_passAllWeather(
+    listAllWeather,
+    SW_Weather.allHist,
+    SW_Model.startyr,
+    SW_Weather.n_years
+  );
+
+
+  // Impute missing values
+  imputeMissingWeather(
+    SW_Weather.allHist,
+    SW_Model.startyr,
+    SW_Weather.n_years,
+    (Bool) SW_Weather.use_weathergenerator,
+    3 // nMaxLOCF
+  );
+
+
+  // Scale with monthly additive/multiplicative parameters
+  scaleAllWeather(
+    SW_Weather.allHist,
+    SW_Model.startyr,
+    SW_Weather.n_years,
+    SW_Weather.scale_temp_max,
+    SW_Weather.scale_temp_min,
+    SW_Weather.scale_precip
+  );
+}
+
+
+static void rSW2_passAllWeather(
+  SEXP listAllW,
+  SW_WEATHER_HIST **allHist,
+  int startYear,
+  unsigned int n_years
+) {
+  int nList, yearIndex, year, numDaysYear, day, doy, i, j;
+  Bool weth_found;
+  SEXP tmpW, yrWData;
+  double *p_yrWData;
+
+  nList = LENGTH(listAllW);
+
+  // Loop over years and move weather data to `SOILWAT2` `allHist`
+  for (yearIndex = 0; yearIndex < n_years; yearIndex++) {
+
+    if (SW_Weather.use_weathergenerator_only) {
+      // Set values to missing for call to `imputeMissingWeather()`
+      _clear_hist_weather(allHist[yearIndex]);
+
+    } else {
+      year = yearIndex + startYear;
+
+      // Locate suitable year among rSOILWAT2 list of `swWeatherData`
+      weth_found = swFALSE;
+
+      for (i = 0; !weth_found && i < nList; i++) {
+        tmpW = VECTOR_ELT(listAllW, i);
+        weth_found = (Bool) year == *INTEGER(GET_SLOT(tmpW, install("year")));
+      }
+
+      if (weth_found) {
+        yrWData = GET_SLOT(tmpW, install("data"));
+        weth_found = !isnull(yrWData);
+      }
+      if (weth_found) {
+        numDaysYear = Time_get_lastdoy_y(year);
+
+        if (nrows(yrWData) != numDaysYear || ncols(yrWData) != 4) {
+          LogError(
+            logfp,
+            LOGFATAL,
+            "Weather data (year %d): wrong dimensions: "
+            "expected %d rows (had %d) and 4 columns (had %d).\n",
+            year,
+            numDaysYear,
+            nrows(yrWData),
+            ncols(yrWData)
+          );
+          return;
+        }
+
+        p_yrWData = REAL(yrWData);
+
+        // Loop over days of current year
+        for (day = 0; day < numDaysYear; day++) {
+
+            doy = p_yrWData[day + numDaysYear * 0];
+            if (doy < 1 || doy > numDaysYear) {
+              LogError(
+                logfp,
+                LOGFATAL,
+                "Weather data (year %d): "
+                "day of year out of range (%d), expected: %d.\n",
+                year,
+                doy,
+                day + 1
+              );
+            }
+
+            // Copy weather data to `SOILWAT2` `allHist`
+
+            // Translate `NA` to `SW_MISSING`
+            j = day + numDaysYear * 1;
+            allHist[yearIndex]->temp_max[day] =
+              (R_FINITE(p_yrWData[j]) && !missing(p_yrWData[j])) ?
+              p_yrWData[j] :
+              SW_MISSING;
+
+            j = day + numDaysYear * 2;
+            allHist[yearIndex]->temp_min[day] =
+              (R_FINITE(p_yrWData[j]) && !missing(p_yrWData[j])) ?
+              p_yrWData[j] :
+              SW_MISSING;
+
+            j = day + numDaysYear * 3;
+            allHist[yearIndex]->ppt[day] =
+              (R_FINITE(p_yrWData[j]) && !missing(p_yrWData[j])) ?
+              p_yrWData[j] :
+              SW_MISSING;
+
+
+            // Calculate average air temperature
+            if (
+              !missing(allHist[yearIndex]->temp_max[day]) &&
+              !missing(allHist[yearIndex]->temp_min[day])
+            ) {
+              allHist[yearIndex]->temp_avg[day] = (
+                allHist[yearIndex]->temp_max[day] +
+                allHist[yearIndex]->temp_min[day]
+              ) / 2.;
+            }
+        }
+
+      } else {
+        // Set values to missing for call to `imputeMissingWeather()`
+        _clear_hist_weather(allHist[yearIndex]);
+      }
     }
-
-    readAllWeather(w->allHist, w->yr.first, w->n_years);
-
+  }
 }
