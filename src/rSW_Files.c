3c572aadf5ab58797c440ebe56471d578fe0391c
Nicholas Persley <nspersley20@gmail.com>
2023-06-20 11:04:42 -0400

    Convert program to use variables inside `PATH_INFO`

--- src/rSW_Files.c
+++ src/rSW_Files.c
@@ -49,19 +49,19 @@ SEXP onGet_SW_F(void) {
 	PROTECT(swFiles = MAKE_CLASS("swFiles"));
 	PROTECT(SW_F_construct = NEW_OBJECT(swFiles));
 	PROTECT(ProjDir = allocVector(STRSXP, 1));
-	SET_STRING_ELT(ProjDir, 0, mkChar(_ProjDir));
+	SET_STRING_ELT(ProjDir, 0, mkChar(PathInfo._ProjDir));
 
 	PROTECT(FilesIn = allocVector(STRSXP, SW_NFILES));
 	for (i = 0; i < SW_NFILES; i++) {
-		if (InFiles[i] != NULL ) {
-			SET_STRING_ELT(FilesIn, i, mkChar(InFiles[i]));
+		if (PathInfo.InFiles[i] != NULL ) {
+			SET_STRING_ELT(FilesIn, i, mkChar(PathInfo.InFiles[i]));
 		}
 	}
 
 	PROTECT(Rweather_prefix = allocVector(STRSXP, 1));
-	SET_STRING_ELT(Rweather_prefix, 0, mkChar(weather_prefix));
+	SET_STRING_ELT(Rweather_prefix, 0, mkChar(PathInfo.weather_prefix));
 	PROTECT(Routput_prefix = allocVector(STRSXP, 1));
-	SET_STRING_ELT(Routput_prefix, 0, mkChar(output_prefix));
+	SET_STRING_ELT(Routput_prefix, 0, mkChar(PathInfo.output_prefix));
 	// attaching main's elements
 	SET_SLOT(SW_F_construct, install(cSW_F_construct_names[0]), ProjDir);
 	SET_SLOT(SW_F_construct, install(cSW_F_construct_names[1]), FilesIn);
@@ -80,22 +80,22 @@ void onSet_SW_F(SEXP SW_F_construct) {
 	SEXP Routput_prefix;
 
 	PROTECT(ProjDir = GET_SLOT(SW_F_construct, install("ProjDir")));
-	strcpy(_ProjDir, CHAR(STRING_ELT(ProjDir,0)));
+	strcpy(PathInfo._ProjDir, CHAR(STRING_ELT(ProjDir,0)));
 
 	PROTECT(FilesIn = GET_SLOT(SW_F_construct, install("InFiles")));
 	j = LENGTH(FilesIn);
 	for(i=0;i<SW_NFILES;i++)
-		if (!isnull(InFiles[i])) {
-			Mem_Free(InFiles[i]);
+		if (!isnull(PathInfo.InFiles[i])) {
+			Mem_Free(PathInfo.InFiles[i]);
 		}
 	for (i = 0; i < j; i++) {
-		InFiles[i] = Str_Dup(CHAR(STRING_ELT(FilesIn,i)));
+		PathInfo.InFiles[i] = Str_Dup(CHAR(STRING_ELT(FilesIn,i)));
 	}
 
 	PROTECT(Rweather_prefix = GET_SLOT(SW_F_construct, install("WeatherPrefix")));
-	strcpy(weather_prefix, CHAR(STRING_ELT(Rweather_prefix,0)));
+	strcpy(PathInfo.weather_prefix, CHAR(STRING_ELT(Rweather_prefix,0)));
 
 	PROTECT(Routput_prefix = GET_SLOT(SW_F_construct, install("OutputPrefix")));
-	strcpy(output_prefix, CHAR(STRING_ELT(Routput_prefix,0)));
+	strcpy(PathInfo.output_prefix, CHAR(STRING_ELT(Routput_prefix,0)));
 	UNPROTECT(4);
 }
