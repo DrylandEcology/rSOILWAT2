295c7e3b0b4abf62a31c00b709fffc7a9717b956
niteflyunicorns <savannahj1200@gmail.com>
2024-03-03 17:28:06 -0700

    Renamed rSW_Model.h to rSW_Domain.h and added appropriate function prototypes

---
+++ src/rSW_Domain.h
@@ -0,0 +1,9 @@
+#include <Rinternals.h> // for SEXP
+
+/* =================================================== */
+/*             Global Function Declarations            */
+/* --------------------------------------------------- */
+SEXP onGet_SW_SPINUP(void);
+SEXP onGet_SW_MDL(void);
+void OnSet_SW_SPINUP(SEXP SW_DOM, LOG_INFO* LogInfo)
+void onSet_SW_MDL(SEXP SW_MDL, LOG_INFO* LogInfo);
